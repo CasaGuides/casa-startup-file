31312fe32ec875840489c72d01381a70fc6122cd
CasaGuides <141352370+CasaGuides@users.noreply.github.com>
2025-10-23 19:04:43 -0400

    Update startup.py

--- startup.py
+++ startup.py
@@ -1,8 +1,7 @@
-
 import sys
+
 sys.path.append('/home/casa/contrib/bitbucket/AIV/analysis_scripts')
 # or update the path to the directory of your extracted tar ball
 
 import analysisUtils as au
 import analysisUtils as aU
-
